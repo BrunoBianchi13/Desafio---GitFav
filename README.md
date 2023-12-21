843821d3c8129fc4d450488c39deddc184dfff25
Bruno Bianchi <bruno.bianchi2000@outlook.com>
2023-12-21 14:10:18 -0300

    version 1.0

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align="center"> Desafio - GitFav </h1>
+
+<p align="center">
+Programa exclusivo do Bruno Bianchi, para ensino de tecnologias WEB. <br/>
+Desafio proposto pela Rocketseat, A ideia agora é criar o GitFav!, um sistema para favoritar perfis do Github. 
+
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>
+<p align="center">
+  
+</p>
+<p align="center">
+  <img alt="" src="./assets/gitfav.png"><img alt="" src="./imagens/resmob.jpeg">
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- Javascript
+- Git e Github
+
+
+## Layout
+
+Link do layout usado no projeto:
+
+- [Layout Figma](https://www.figma.com/file/SzQA07HwmSPj4hOYgu1Pps/%5BDesafios-Explorer%5D-GitFav-(Copy)/duplicate?type=design&node-id=104-48&mode=design)
+
+## 💻 Projeto
+
+
+- [Acesse o projeto finalizado, online](https://brunobianchi13.github.io/Desafio-GitFav/)
+
+
+---
+
+<p align="center">
+Feito by Bruno Bianchi
+</p>
---
+++ js/favorites.js
@@ -0,0 +1,160 @@
+export class GithubUser{
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+    .then(data => data.json())
+    .then(({login, name, public_repos, followers}) => ({
+      login,
+      name,
+      public_repos,
+      followers
+    }))
+  }
+}
+
+
+export class Favorites {
+  constructor(root) {
+      this.root = document.querySelector(root)
+      this.load()
+
+      
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [] 
+
+  }
+
+  save(){
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username){
+    
+
+    try {
+
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if (userExists) {
+        throw new Error('Usuario ja existe no sistema')
+      }
+      const user = await GithubUser.search(username)
+
+      if (user.login === undefined) {
+        throw new Error('Usuario não encontrado')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
+    }
+    
+    
+
+
+
+  }
+
+
+  
+
+  delete(user) {
+    console.log(user)
+    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+    console.log(filteredEntries)
+    this.entries = filteredEntries
+    this.update()
+
+    this.save()
+  }
+
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+      super(root)
+      this.tbody = this.root.querySelector('table tbody')
+
+      this.update()
+      this.onadd()
+  }
+
+  onadd(){
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
+  }
+
+  update() {
+
+    this.removeAllTr()
+
+      this.entries.forEach( user => {
+        const row = this.createRow()
+
+        row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+
+            row.querySelector('.remove').onclick = () => {
+              const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+              console.log(isOk)
+              if(isOk) {
+                  this.delete(user)
+              }
+          }
+        this.tbody.append(row)
+      })
+
+      
+
+  }
+
+
+  createRow(){
+
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+      
+      <td class="user">
+        <img src="https://github.com/BrunoBianchi13.png" alt="">
+        <a href="https://github.com/BrunoBianchi13" target="_blank">
+          <p>Bruno Bianchi</p>
+          /<span>brunobianchi13</span>
+        </a>
+      </td>
+      <td class="repositories">76</td>
+      <td class="followers">9589</td>
+      <td>
+        <button class="remove">remove</button>
+      </td>
+   
+      
+      
+      `
+
+
+      return tr
+  }
+  removeAllTr() {
+      
+
+      this.tbody.querySelectorAll('tr').forEach((tr) => {
+          tr.remove()
+      });
+  }
+}
